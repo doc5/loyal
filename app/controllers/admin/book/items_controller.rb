c4a4eec086aa838e3c0f67471cbae1e107a838ce
WangJinzhong <wangjinzhong@rd.tuan800.com>
2012-01-06 16:47:17 +0800

    item admin

--- app/controllers/admin/book/items_controller.rb
+++ app/controllers/admin/book/items_controller.rb
@@ -1,2 +1,61 @@
 class Admin::Book::ItemsController < AdminController
+  def index
+    @book_items = BookItem.paginate(:page => params[:page])
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def show
+    @book_item = BookItem.find(params[:id])
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def new
+    @book_item = BookItem.new
+
+    respond_to do |format|
+      format.html 
+    end
+  end
+
+  def edit
+    @book_item = BookItem.find(params[:id])
+  end
+  
+  def destroy
+    @book_item = BookItem.find(params[:id])
+    @book_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_route(admin_book_items_path) }
+    end
+  end
+  
+  def update
+    @book_item = BookItem.find(params[:id])
+
+    respond_to do |format|
+      if @book_item.update_attributes(params[:book_item])        
+        format.html { redirect_to admin_route(admin_book_item_path(@book_item)), :notice => 'Book Item was successfully updated.' }
+      else
+        format.html { render :action => "edit" }
+      end
+    end  
+  end
+  
+  def create
+    @book_item = BookItem.new(params[:book_item])    
+    
+    respond_to do |format|
+      if @book_item.save
+        format.html { redirect_to admin_route(admin_book_item_path(@book_item)), :notice => 'Book Item was successfully created.' }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -5,11 +5,14 @@ class Role < ActiveRecord::Base
 
   
   has_many :assignments
-  has_many :users, :through => :assignments
+  has_many :users, :through => :assignments  
   
   validates_presence_of :name, :remark
   validates_uniqueness_of :name, :case_sensitive => false
   
+  before_save do |r|
+    r.name.downcase! unless r.name.nil?
+  end
   
   def display_with_remark
     "#{name.humanize}(#{remark})"
