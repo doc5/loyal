e66030ca69ca8be8c11cfc2b1c84005d95261e3d
happy <happy@doc5.com>
2012-02-04 02:12:51 +0800

    archives categories view

--- app/controllers/yuedu123/archives/categories_controller.rb
+++ app/controllers/yuedu123/archives/categories_controller.rb
@@ -1,6 +1,18 @@
 class Yuedu123::Archives::CategoriesController < Yuedu123Controller
   def show
     @archives_category = ArchivesCategory.find_by_url_name params[:url_name]
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def index
+    @archives_categories = ArchivesCategory.total_tree_struct
+    
+    respond_to do |format|
+      format.html
+    end
   end
 
 end
