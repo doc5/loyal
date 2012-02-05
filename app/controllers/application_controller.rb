a38892dcdf2fbc661e25e8162bea539c3fc19341
happy <happy@doc5.com>
2012-02-05 22:11:38 +0800

    mobile fu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,26 @@ class ApplicationController < ActionController::Base
   include ControllerExt::Authenticated
   include ControllerExt::DomainRoute
   include ControllerExt::Languaged
+  include March::Tools::SystemTools
   
   protect_from_forgery
   
+  around_filter :memory_look
+
+#  around_filter :record_memory  
+#  def record_memory  
+#    process_status = File.open("/proc/#{Process.pid}/status")  
+#    13.times { process_status.gets }  
+#    rss_before_action = process_status.gets.split[1].to_i  
+#    process_status.close  
+#    yield  
+#    process_status = File.open("/proc/#{Process.pid}/status")  
+#    13.times { process_status.gets }  
+#    rss_after_action = process_status.gets.split[1].to_i  
+#    process_status.close  
+#    logger.info("===============>: CONSUME MEMORY: #{rss_after_action - rss_before_action} \KB\tNow: #{rss_after_action} KB\t#{request.url}")  
+#  end 
   
-  def render_404
-    
-  end
+#  def render_404    
+#  end
 end
---
+++ extras/march/tools/system_tools.rb
@@ -0,0 +1,26 @@
+# -*- encoding : utf-8 -*-
+module March
+  module Tools
+    module SystemTools
+      def memory_look
+        process_status = File.open("/proc/#{Process.pid}/status")  
+        13.times { process_status.gets }  
+        rss_before_action = process_status.gets.split[1].to_i  
+        process_status.close  
+        
+        res = nil
+        if block_given?
+          res = yield
+        end
+          
+        process_status = File.open("/proc/#{Process.pid}/status")  
+        13.times { process_status.gets }  
+        rss_after_action = process_status.gets.split[1].to_i  
+        process_status.close 
+
+        puts "==============> CONSUME MEMORY: #{rss_after_action - rss_before_action} \KB\tNow: #{rss_after_action} KB"
+        res
+      end
+    end
+  end
+end
