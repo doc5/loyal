d1749cba844da1b78df677bafa01a07accd51b6d
happy <happy@doc5.com>
2012-02-07 00:13:55 +0800

    asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Loyal::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-#  config.serve_static_assets = false
   config.serve_static_assets = true
+#  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -43,10 +43,12 @@ Loyal::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "http://assets.doc5.com"
+  config.action_controller.asset_host = "http://assets.doc5.com:3000"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin.js, apps.js, blogsoso.js docfive.js home.js map.js sessions.js tip.js users.js yuedu123.js temporary.js)
+  config.assets.precompile += %w( admin.css, apps.css, blogsoso.css docfive.css home.css map.css sessions.css tip.css users.css yuedu123.css temporary.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
