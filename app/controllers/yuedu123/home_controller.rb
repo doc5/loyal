2ba3a6d8d6e99443aebad5efcf33b0141d4a25af
happy <happy@doc5.com>
2012-02-12 14:03:12 +0800

    search with google

--- app/controllers/yuedu123/home_controller.rb
+++ app/controllers/yuedu123/home_controller.rb
@@ -7,20 +7,20 @@ class Yuedu123::HomeController < Yuedu123Controller
   
 #  文章搜索
   def search
-    @query_words = params[:query]
-    
-    if @query_words.present?
-      @query_words_used = RMMSeg::SimpleAlgorithm.new(@query_words).segment.join(" ")
-      @query_results = ActsAsFerret.find(
-        @query_words_used, 
-        'shared', 
-        {:page => params[:page], :per_page => 20, :lazy => true}, 
-        {}
-      )
-      
-      @seo_title = "阅读搜索_#{@query_words} - #{Website::Info.yuedu123_name}"
-    else
-      @seo_title = "阅读搜索 - #{Website::Info.yuedu123_name}"
-    end
+#    @query_words = params[:query]
+#    
+#    if @query_words.present?
+#      @query_words_used = RMMSeg::SimpleAlgorithm.new(@query_words).segment.join(" ")
+#      @query_results = ActsAsFerret.find(
+#        @query_words_used, 
+#        'shared', 
+#        {:page => params[:page], :per_page => 20, :lazy => true}, 
+#        {}
+#      )
+#      
+#      @seo_title = "阅读搜索_#{@query_words} - #{Website::Info.yuedu123_name}"
+#    else
+#      @seo_title = "阅读搜索 - #{Website::Info.yuedu123_name}"
+#    end
   end
 end
